0d494dcc87d1e29d5a04efd134c9a0f423d73063
Gabriel Lanza <gabriel-lanza21@outlook.com>
2025-12-06 00:20:08 -0300

    feat(api): Criando BaseController + ClienteController (Listar)

---
+++ src/OrdersCustomers/Controllers/BaseController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrdersCustomers.Application.ViewModels;
+using OrdersCustomers.Domain.Entities.Comum;
+using OrdersCustomers.Domain.Interfaces.Comum;
+
+namespace OrdersCustomers.Controllers;
+
+public abstract class BaseController : ControllerBase
+{
+    protected readonly INotificationService Notifications;
+
+    protected BaseController(IServiceProvider serviceProvider)
+    {
+        Notifications = serviceProvider.GetService<INotificationService>();
+    }
+
+    
+    #region Notifications
+
+    protected void NewNotification(string key, string message, NotificationType type = NotificationType.Error) => Notifications.NewNotification(key, message, type);
+
+    protected void NotificationErrors<TEntity>(TEntity model) where TEntity : IModelValidator? => Notifications.NotificationErrors(model);
+
+    #endregion
+
+    protected bool OperacaoValida()
+    {
+        return !Notifications.HasNotificationsErrors();
+    }
+}
+
+public abstract class ApiBaseController : BaseController
+{
+    protected ApiBaseController(IServiceProvider serviceProvider) : base(serviceProvider)
+    {
+    }
+
+    protected IActionResult ResponseGeneric<T>(T result)
+    {
+        if (!Notifications.HasNotificationsErrors())
+        {
+            return Ok(new ViewModel<T>(result, Notifications.GetNotifications()));
+        }
+
+        return BadRequest(new ViewModel<T>
+        (
+            path: Request.Path.Value,
+            remoteAddress: HttpContext?.Connection.RemoteIpAddress?.ToString(),
+            notifications: Notifications.GetNotifications()
+        ));
+    }
+
+    protected IActionResult CreateResponse<T>(T result) where T : IComparable
+    {
+        if (!Notifications.HasNotifications())
+        {
+            return Created("", result);
+        }
+
+        if (OperacaoValida())
+        {
+            return Accepted(new ViewModel<T>
+            (
+                notificationError: Notifications.GetNotifications()
+            ));
+        }
+
+        return BadRequest(new ViewModel<T>
+        (
+            path: Request.Path.Value,
+            remoteAddress: HttpContext?.Connection.RemoteIpAddress?.ToString(),
+            notifications: Notifications.GetNotifications()
+        ));
+    }
+
+    protected IActionResult Response<T>(T result) where T : IComparable => ResponseGeneric(result);
+
+    protected IActionResult Response<T>(IEnumerable<T> result) where T : IComparable => ResponseGeneric(result);
+
+    protected IActionResult Response(IEnumerable<dynamic> result) => ResponseGeneric(result);
+
+}
---
+++ src/OrdersCustomers/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrdersCustomers.Domain.Interfaces;
+
+namespace OrdersCustomers.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClienteController : ApiBaseController
+{
+    private readonly IClienteService _clienteService;
+
+    public ClienteController(IServiceProvider serviceProvider, IClienteService clienteService) : base(serviceProvider)
+    {
+        _clienteService = clienteService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Listar()
+    {
+        var ret = await _clienteService.Listar();
+
+        return Ok(ret);
+    }
+
+    
+
+}
